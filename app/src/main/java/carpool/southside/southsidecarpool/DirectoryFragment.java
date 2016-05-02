d1daef538a8d3b06a02598864c1257658b58303a
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-05-02 11:04:01 +0800

    Southside Carpool Alpha 0.3
    - Swipeable Items in Directory! -

--- app/src/main/java/carpool/southside/southsidecarpool/DirectoryFragment.java
+++ app/src/main/java/carpool/southside/southsidecarpool/DirectoryFragment.java
@@ -1,10 +1,17 @@
 package carpool.southside.southsidecarpool;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +24,7 @@ public class DirectoryFragment extends Fragment implements RadioGroup.OnCheckedC
     private RecyclerView rvPeople;
     private PersonAdapter personAdapter;
     private SegmentedGroup segmentedSchool;
+    private Paint p = new Paint();
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.directory_view, container, false);
         segmentedSchool = (SegmentedGroup) v.findViewById(R.id.segmented_directory);
@@ -30,6 +38,7 @@ public class DirectoryFragment extends Fragment implements RadioGroup.OnCheckedC
         personAdapter = new PersonAdapter(people);
         rvPeople.setAdapter(personAdapter);
         rvPeople.setLayoutManager(new LinearLayoutManager(v.getContext()));
+        initSwipe();
         return v;
     }
     @Override
@@ -46,5 +55,51 @@ public class DirectoryFragment extends Fragment implements RadioGroup.OnCheckedC
                 break;
             default:
         }
+    }
+    private void initSwipe(){
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target){
+                return false;
+            }
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                if(direction == ItemTouchHelper.LEFT){
+                    Toast.makeText(getActivity(), "Swiped Left", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getActivity(), "Swiped Right", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive){
+                Bitmap icon;
+                if(actionState == ItemTouchHelper.ACTION_STATE_SWIPE){
+                    View itemView = viewHolder.itemView;
+                    float height = (float) itemView.getBottom() - (float) itemView.getTop();
+                    float width = height / 3;
+                    if(dX > 0){
+                        p.setColor(Color.parseColor("#4CAF50"));
+                        RectF background = new RectF((float) itemView.getLeft(), (float) itemView.getTop(), dX,(float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.call);
+                        RectF icon_dest = new RectF((float) itemView.getLeft() + width ,(float) itemView.getTop() + width,(float) itemView.getLeft()+ 2*width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                    else{
+                        p.setColor(Color.parseColor("#FFC107"));
+                        RectF background = new RectF((float) itemView.getRight() + dX, (float) itemView.getTop(),(float) itemView.getRight(), (float) itemView.getBottom());
+                        c.drawRect(background,p);
+                        icon = BitmapFactory.decodeResource(getResources(), R.drawable.message);
+                        RectF icon_dest = new RectF((float) itemView.getRight() - 2*width ,(float) itemView.getTop() + width,(float) itemView.getRight() - width,(float)itemView.getBottom() - width);
+                        c.drawBitmap(icon,null,icon_dest,p);
+                    }
+                }
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(rvPeople);
     }
 }
