d9c0437d85ddd8d0dd638ee9c0947ece25ba0dbe
Carlo Gabriel Villalon Tapales <carlogabrielvillalon@gmail.com>
2016-05-02 11:08:03 +0800

    Southside Carpool Alpha 0.3.1
    - Added Shift Day -

--- app/src/main/java/carpool/southside/southsidecarpool/Shift.java
+++ app/src/main/java/carpool/southside/southsidecarpool/Shift.java
@@ -3,20 +3,24 @@ package carpool.southside.southsidecarpool;
 public class Shift{
     public static final String TABLE_NAME = "Shift";
     public static final String COL_ID = "shiftID";
+    public static final String COL_DAY = "shiftDay";
     public static final String COL_TYPE = "shiftType";
     public static final String COL_TIME = "shiftTime";
     public static final String COL_PROVIDER = "shiftProvider";
     private int shiftID;
+    private String shiftDay;
     private String shiftType; //SchoolBound or Homebound
     private String shiftTime;
     private String shiftProvider;
-    public Shift(String shiftType, String shiftTime, String shiftProvider){
+    public Shift(String shiftDay, String shiftType, String shiftTime, String shiftProvider){
+        this.shiftDay = shiftDay;
         this.shiftType = shiftType;
         this.shiftTime = shiftTime;
         this.shiftProvider = shiftProvider;
     }
-    public Shift(int shiftID, String shiftType, String shiftTime, String shiftProvider){
+    public Shift(int shiftID, String shiftDay, String shiftType, String shiftTime, String shiftProvider){
         this.shiftID = shiftID;
+        this.shiftDay = shiftDay;
         this.shiftType = shiftType;
         this.shiftTime = shiftTime;
         this.shiftProvider = shiftProvider;
@@ -26,6 +30,12 @@ public class Shift{
     }
     public void setShiftID(int shiftID){
         this.shiftID = shiftID;
+    }
+    public String getShiftDay(){
+        return shiftDay;
+    }
+    public void setShiftDay(String shiftDay){
+        this.shiftDay = shiftDay;
     }
     public String getShiftType(){
         return shiftType;
